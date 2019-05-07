d730f3a93b9a91b1517fb7ed2b8408f5bfb81923
John Oro <johnmichaeloro@gmail.com>
2019-05-07 09:27:46 -0600

    app working

--- top-news/src/TopHeadlines/TopHeadlines.jsx
+++ top-news/src/TopHeadlines/TopHeadlines.jsx
@@ -4,22 +4,33 @@ class TopHeadlines extends Component {
   constructor(){
     super();
     this.state = {
-      headline: ''
+      headline: '',
+      author: '',
+      description: ''
     }
   }
-  showHeadlines = async () => {
+  showHeadlines = async (e) => {
+    e.preventDefault();
     const newsURL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=b6ae8688af7b4d5aae22e0dc8c4fa6f3';
     const result = await fetch(newsURL);
     const parsedResult = await result.json();
+    console.log(parsedResult);
     this.setState({
-      headline: parsedResult.articles[0].title
+      headline: parsedResult.articles[0].title,
+      author: parsedResult.articles[0].author,
+      description: parsedResult.articles[0].description
     })
   }
   render(){
     return(
       <div>
+        <form onSubmit={this.showHeadlines}>
+          Show me the headline! <input type='submit' />
+        </form>
         <h1>Today's top headline is</h1>
-        <h2>{this.state.headline}</h2>
+        <h2>Headline: {this.state.headline}</h2>
+        <h2>Author: {this.state.author}</h2>
+        <h2>Description: {this.state.description}</h2>
       </div>
     )
   }
